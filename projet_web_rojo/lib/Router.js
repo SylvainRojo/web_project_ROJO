26e7c61b10ee9469b616f6b48afdf3e70e392240
Sylvain Rojo <Sylvain Rojo>
2016-11-25 16:06:44 +0100

    Quatrième Commit

--- projet_web_rojo/lib/Router.js
+++ projet_web_rojo/lib/Router.js
@@ -3,7 +3,17 @@ Router.configure({
 });
 
 Router.route('/', {
-	name: 'accueil'
+	name: "home",
+	data: function(){
+		var posts = Posts.find();
+
+		return {
+			posts: posts
+		};
+	},
+	waitOn: function(){
+		return Meteor.subscribe("allPostHearders");
+	}
 });
 
 Router.route('/posts', {
